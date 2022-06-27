5ccaa3e1ab045bced384ed9ed298868c58e0de46
Bernardo <bernardo_ruizesquide@hotmail.com>
2022-06-27 19:15:37 -0300

    06

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -9,10 +9,44 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post('/product', (req, res) => {
+app.get('/test', (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    message: 'test',
   });
 });
+
+app.post('/sum', (req, res) => {
+  const { a, b } = req.body
+  res.send({
+    result: a + b
+  });
+});
+
+app.post('/product', (req, res) => {
+  const { a, b } = req.body
+  res.send({ result: a * b });
+});
+
+app.post('/sumArray', (req, res) => {
+  const { array, num } = req.body
+  if (typeof num === 'number') {
+    for (i = 0; i < array.length - 1; i++) {
+      for (j = i + 1; j < array.length; j++) {
+        if (array[i] + array[j] === num) {
+          return res.status(200).json({ result: true })
+        }
+      }
+    }
+  }
+  return res.status(400).json({ result: false, });
+});
+
+app.post('/numString', (req, res) => {
+  const {string} = req.body
+  if (typeof string === 'string' && string) {
+   return res.status(200).json({result: string.length})
+  }
+  return res.status(400).json(false)
+})
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
